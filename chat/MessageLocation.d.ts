fa3674433a84eeb463cc5bca45df7cd50a056b59
houxh <houxuehua49@gmail.com>
2021-03-23 04:11:22 +0800

    MessageLocation decalre

--- chat/MessageLocation.d.ts
+++ chat/MessageLocation.d.ts
@@ -1,12 +1,2 @@
-//export * from "./MessageLocation.ios";
-
-
-
-import React from 'react';
-
-
-import {Message as IMessage} from "../model/IMessage";
-
-export  default class MessageLocation extends React.Component<{currentMessage:IMessage}, {}> {
-}
-
+import MessageLocation from "./MessageLocation.ios";
+export default MessageLocation;
